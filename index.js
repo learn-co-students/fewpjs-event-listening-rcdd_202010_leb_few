4c980811077e07bac32e6bf14f572ca182f22b34
Charbel t <charbel2000.52@gmail.com>
2020-11-15 13:10:09 +0000

    Done.

--- index.js
+++ index.js
@@ -1,2 +1,11 @@
-function addingEventListener() {
+const input = document.getElementById('input');
+
+function addingEventListener(event) {
+  const input = document.getElementById('input');
+input.addEventListener('click', function(event) {
+  alert('I was clicked!');
+});
+  
 }
+
+//input.addEventListener('click',addingEventListener(event));
